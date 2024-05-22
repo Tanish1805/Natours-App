f6e4fcbf4e07603953c9992a054603a0031d2306
Tanish1805 <tanishchugh05@gmail.com>
2024-05-23 01:13:11 +0530

    Creating a login route to render login pug template

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -9,5 +9,6 @@ const router = express.Router();
 router.get('/', viewsController.getOverview);
 
 router.get('/tour/:slug', viewsController.getTour);
+router.get('/login', viewsController.getLoginForm);
 
 module.exports = router;
