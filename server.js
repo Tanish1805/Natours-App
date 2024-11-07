06fc579681e7ea81786b10660783f6a5d239ad45
Tanish1805 <tanishchugh05@gmail.com>
2024-11-08 03:39:53 +0530

    Handling the sigterm for render

--- server.js
+++ server.js
@@ -31,11 +31,23 @@ const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
 
+// Here basically on unhandledrejection we gracefull shut down the app
 process.on('unhandledRejection', (err) => {
   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
   console.log(err.name, err.message);
   // Usually we first shut down the server and then the application. SMOOTH
   server.close(() => {
     process.exit(1);
+  });
+});
+
+// Similar to what we did above we'll do the same for our deployment
+// Render basically sends a sigterm signal which shuts down the app abruptly, we want to shut down gracefully
+// Basically it will first handle the pending request and then shut down gracefully
+process.on('SIGTERM', () => {
+  console.log('SIGTERM RECIEVED, Shutting down gracefully');
+  server.close(() => {
+    console.log('💥 Process terminated...');
+    // Here we do not use process.exit, because the sigterm will automatically shut down the process
   });
 });
